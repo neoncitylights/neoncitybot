ccb5c16a1921a6015d289a167c717a42604c7407
Samantha <sam.t.nguyenn@gmail.com>
2022-12-10 17:52:08 -0600

    feat: add /ratelimit slash command

--- src/commands/CommandRegistry.ts
+++ src/commands/CommandRegistry.ts
@@ -1,8 +1,10 @@
 import { Command } from './Command';
 import { ghIssue } from './ghIssue';
 import { ghUser } from './ghUser';
+import { rateLimit } from './rateLimit';
 
 export const CommandRegistry: Command[] = [
 	ghIssue,
-	ghUser
+	ghUser,
+	rateLimit,
 ];

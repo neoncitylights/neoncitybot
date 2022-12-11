691e4b0452dca4c4ff968f5757e7a33dacdc3d86
Samantha <sam.t.nguyenn@gmail.com>
2022-12-10 23:14:20 -0600

    feat: improve overall /ratelimit command

--- src/commands/rateLimit.ts
+++ src/commands/rateLimit.ts
@@ -6,20 +6,11 @@ import { components } from '@octokit/openapi-types';
 export const rateLimit: Command = {
 	data: new SlashCommandBuilder()
 		.setName('ratelimit')
-		.setDescription('Get Rate limit information for REST API clients')
-		.addStringOption(option => option
-			.setName('client')
-			.setDescription('The REST API client to query for')
-			.setRequired(true)
-			.addChoices(
-				{ name: 'GitHub', value: 'github' },
-				{ name: 'Discord', value: 'discord' }
-			)),
+		.setDescription('Get rate limit information for the GitHub API endpoints'),
 
 	run: async (client: BotClient, interaction: CommandInteraction) => {
 		if(!interaction.isChatInputCommand()) return;
 
-		const givenClient = interaction.options.getString('client') as 'github' | 'discord';
 		const gh = client.githubClient;
 		const ghRateLimit = await gh.rest.rateLimit.get();
 		const ghRateLimitData = ghRateLimit.data;
@@ -35,17 +26,16 @@ export const rateLimit: Command = {
 const formatRateLimit = (rateLimit: components['schemas']['rate-limit']) => {
 	const { limit, remaining, reset, used } = rateLimit;
 	const embed = new EmbedBuilder()
-		.setTitle(':stopwatch: Rate Limit')
+		.setTitle(':stopwatch: GitHub Rate Limit')
 		.setDescription(`You have ${remaining} of ${limit} requests remaining.`)
+		.setThumbnail('https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png')
 		.addFields(
 			{ name: 'Limit', value: limit.toString() + " requests allowed per hour", inline: true },
 			{ name: 'Used', value: used.toString() + " requests made", inline: true },
 			{ name: 'Remaining', value: remaining.toString() + " requests remaining", inline: true },
-			{ name: 'Reset', value: new Date(reset * 1000).toLocaleString(), inline: true },
 		)
 		.setFooter({
-			text: 'GitHub API',
-			iconURL: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png'
+			text: `Resets at ${new Date(reset * 1000).toLocaleString()}.`,
 		})
 	return embed;
 }
